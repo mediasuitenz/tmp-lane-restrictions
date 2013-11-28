d78d586948f0fd8c456970fe39335c5ff80a532e
Richard Walker <digitalsadhu@gmail.com>
2013-11-28 16:55:57 +0100

    Finish endpoint for returning lane restrictions

--- modules/api/controllers/LanerestrictionsController.php
+++ modules/api/controllers/LanerestrictionsController.php
@@ -12,13 +12,38 @@ class LanerestrictionsController extends Controller {
 
             $type = Yii::app()->request->getQuery('type');
             $typeId = Yii::app()->request->getQuery('type_id');
+            $typeIds = Yii::app()->request->getQuery('type_ids');
+
+            if (empty($type)) {
+                ApiUtils::jsonError($app, 400, 'You must specify type param');
+            }
+
+            if (empty($typeId) && empty($typeIds)) {
+                ApiUtils::jsonError($app, 400,
+                    'You must specify either typeId or typeIds param'
+                );
+            }
+
+            if (false === empty($typeId) && false === empty($typeIds)) {
+                ApiUtils::jsonError($app, 400,
+                    'You must not specify both typeId or typeIds params'
+                );
+            }
 
             $laneRestrictions = OsmLaneRestriction::model()
                 ->type($type)
                 ->typeId($typeId)
+                ->typeIds(json_decode($typeIds, true))
                 ->findAll();
 
-            ApiUtils::jsonRender($app, $laneRestrictions);
+            //run the collection through array map to convert objects
+            //to well formed data array
+            $data = array_map(
+                array('LanerestrictionUtils', 'lanerestrictionArray'),
+                $laneRestrictions
+            );
+
+            ApiUtils::jsonRender($app, $data);
 
         });
         return $app;
