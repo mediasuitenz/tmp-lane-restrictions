1d81888ad70b58c43eed54cc18e9d0c5e77f74df
Richard Walker <digitalsadhu@gmail.com>
2013-11-29 14:12:32 +0100

    Tidy controller code for lane restrictions
    See #55

--- modules/api/controllers/LanerestrictionsController.php
+++ modules/api/controllers/LanerestrictionsController.php
@@ -33,124 +33,47 @@ class LanerestrictionsController extends Controller {
 
         $app->get('/lanerestrictions/api/lanerestrictions', function () use ($app) {
 
-            $type = Yii::app()->request->getQuery('type');
-            $typeId = Yii::app()->request->getQuery('type_id');
-            $typeIds = Yii::app()->request->getQuery('type_ids');
-            $nodeId = Yii::app()->request->getQuery('node_id');
-            $nodeIds = Yii::app()->request->getQuery('node_ids');
-            $path = Yii::app()->request->getQuery('path');
-            $paths = Yii::app()->request->getQuery('paths');
-            $hasRestrictions = Yii::app()->request->getQuery('has_restrictions');
+            $request = $app->request();
 
-            if (false === empty($typeId) && false === empty($typeIds)) {
-                ApiUtils::jsonError($app, 400,
-                    'You may specify either type_id or type_ids, not both'
-                );
+            //setup variables from URL
+            $type            = $request->params('type');
+            $typeId          = $request->params('type_id');
+            $typeIds         = $request->params('type_ids');
+            $nodeId          = $request->params('node_id');
+            $nodeIds         = $request->params('node_ids');
+            $path            = $request->params('path');
+            $paths           = $request->params('paths');
+            $hasRestrictions = $request->params('has_restrictions');
+
+            //Perform checks
+            $msg = false;
+            $msg = ApiUtils::checkTypeParams($msg, $type, $typeId, $typeIds);
+            $msg = ApiUtils::checkNodeParams($msg, $nodeId, $nodeIds);
+            $msg = ApiUtils::checkPathParams($msg, $path, $paths);
+            if ($msg) {
+                ApiUtils::jsonError($app, 400, $msg);
             }
 
-            if (false === empty($nodeId) && false === empty($nodeIds)) {
-                ApiUtils::jsonError($app, 400,
-                    'You may specify either node_id or node_ids, not both'
-                );
-            }
-
-            if (false === empty($path) && false === empty($paths)) {
-                ApiUtils::jsonError($app, 400,
-                    'You may specify either path or paths, not both'
-                );
-            }
-
-            if ((false === empty($typeId) || false === empty($typeIds))
-                && empty($type)
-            ) {
-                ApiUtils::jsonError($app, 400,
-                    'You must specify type param with type_id or type_ids param'
-                );
-            }
-
-            if (false === empty($typeIds)) {
-
-                $typeIds = json_decode($typeIds, true);
-                if (false === is_array($typeIds)) {
-                    $message = 'type_ids must be an array eg. ' .
-                        'type_ids=[123,124,125]';
-                    ApiUtils::jsonError($app, 400, $message);
-                }
-
-            }
-
-            if (false === empty($nodeIds)) {
-
-                $nodeIds = json_decode($nodeIds, true);
-
-                if (false === is_array($nodeIds)) {
-                    $message = 'node_ids must be an array eg. ' .
-                        'node_ids=[12312312,12412312,12512312]';
-                    ApiUtils::jsonError($app, 400, $message);
-                }
-
-            }
-
-            if (false === empty($path)) {
-
-                $path = json_decode($path, true);
-
-                if (false === is_array($path)) {
-                    $message = 'path must be an array eg. ' .
-                        'path=[12312312,12341233]';
-                    ApiUtils::jsonError($app, 400, $message);
-                }
-
-                if (false === isset($path[0]) || false === isset($path[1])) {
-                    $message = 'path array must have 2 values eg. ' .
-                        'path=[12312312,12341233]';
-                    ApiUtils::jsonError($app, 400, $message);
-                }
-
-            }
-
-            if (false === empty($paths)) {
-
-                $paths = json_decode($paths, true);
-
-                if (false === is_array($paths)) {
-                    $message = 'paths must be an array eg. ' .
-                        'paths=[[12312312,12341233],[12412312,12341233]]';
-                    ApiUtils::jsonError($app, 400, $message);
-                }
-
-                if (false === is_array($paths[0])) {
-                    $message = 'paths must be an array of arrays eg. ' .
-                        'paths=[[12312312,12341233],[12412312,12341233]]';
-                    ApiUtils::jsonError($app, 400, $message);
-                }
-
-                if (false === isset($paths[0][0]) || false === isset($paths[0][1])) {
-                    $message = 'paths must be an array of integer arrays eg. ' .
-                        'paths=[[12312312,12341233],[12412312,12341233]]';
-                    ApiUtils::jsonError($app, 400, $message);
-                }
-
-            }
-
+            //Fetch lane restriction objects
             $laneRestrictions = OsmLaneRestriction::model()
                 ->type($type)
                 ->typeId($typeId)
-                ->typeIds($typeIds)
+                ->typeIds(json_decode($typeIds, true))
                 ->osmNode($nodeId)
-                ->osmNodes($nodeIds)
-                ->osmPath($path)
-                ->osmPaths($paths)
+                ->osmNodes(json_decode($nodeIds, true))
+                ->osmPath(json_decode($path, true))
+                ->osmPaths(json_decode($paths, true))
                 ->hasRestrictions($hasRestrictions)
                 ->findAll();
 
             //run the collection through array map to convert objects
-            //to well formed data array
+            //to well formed data arrays
             $data = array_map(
                 array('LanerestrictionUtils', 'lanerestrictionArray'),
                 $laneRestrictions
             );
 
+            //render out the result
             ApiUtils::jsonRender($app, $data);
 
         });
