1b82a543d0539e0578c355d739883b2233ca385d
Richard Walker <digitalsadhu@gmail.com>
2013-12-01 18:31:16 +0100

    Update module definition file with module params

--- LanerestrictionsModule.php
+++ LanerestrictionsModule.php
@@ -2,12 +2,13 @@
 
 class LanerestrictionsModule extends CWebModule
 {
+
+    public $localTimeZone;
+    public $serverTimeZone;
+    public $apiDateTimeOutputFormat;
+
     public function init()
     {
-        // this method is called when the module is being created
-        // you may place code here to customize the module or the application
-
-        // import the module-level models and components
         $this->setImport(array(
             'lanerestrictions.models.*',
             'lanerestrictions.components.*',
@@ -21,8 +22,6 @@ class LanerestrictionsModule extends CWebModule
     {
         if(parent::beforeControllerAction($controller, $action))
         {
-            // this method is called before any module controller action is performed
-            // you may place customized code here
             return true;
         }
         else
