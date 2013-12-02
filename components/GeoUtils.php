416f5d7ea38198427655ff5bcbcd65784c93c40a
Richard Walker <digitalsadhu@gmail.com>
2013-12-02 20:49:38 +0800

    Add GeoUtils class w/ method to calc latlng points

---
+++ components/GeoUtils.php
@@ -0,0 +1,42 @@
+<?php
+class GeoUtils {
+
+    /**
+     * Takes an originating latlng coordinate point and returns a new latlng
+     * a given distance away in a given direction (bearing)
+     *
+     * @param  float   $lat      - latitudinal coord
+     * @param  float   $lng      - longitudinal coord
+     * @param  integer $bearing  - direction, possibile values are 0-360 where
+     *                             0 = north, 90 = east, 180 = south, 270 = west
+     * @param  float   $distance - in km away from the originating latlng
+     *
+     * @return array             - array of lat and lng
+     */
+    public static function dueCoords($lat, $lng, $bearing, $distance) {
+
+        $radius = 6378.1;
+
+        //  New lat in degrees.
+        $new_lat = rad2deg(
+            asin(sin(deg2rad($lat)) * cos($distance / $radius) +
+                cos(deg2rad($lat)) * sin($distance / $radius) *
+                    cos(deg2rad($bearing))));
+
+        //  New lng in degrees.
+        $new_lng = rad2deg(deg2rad($lng) +
+            atan2(sin(deg2rad($bearing)) * sin($distance / $radius) *
+                cos(deg2rad($lat)), cos($distance / $radius) -
+                    sin(deg2rad($lat)) * sin(deg2rad($new_lat))));
+
+        $coord = array();
+        $coord['lat'] = $new_lat;
+        $coord['lng'] = $new_lng;
+
+        return $coord;
+
+    }
+}
+
+
+
